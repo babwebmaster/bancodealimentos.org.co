0ddb659fce2457c95b938f3c28ee15bc49352696
Jhonny andres ibañez garzon <jhandres187@gmail.com>
2022-06-07 17:08:24 -0500

    ajustes de styles

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -10,7 +10,7 @@
                     </a>
                 </div>       
                 <!-- Hamburger -->
-                <div class="-mr-2 flex items-center md:hidden ml-auto mr-5">
+                <div class="flex items-center md:hidden ml-auto mr-5">
                     <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-slate-50 hover:text-slate-400 focus:outline-none focus:text-slate-400 transition">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
