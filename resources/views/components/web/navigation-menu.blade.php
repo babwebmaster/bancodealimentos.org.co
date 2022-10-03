dfe9b3505134f3e9294be7113d0396204e58225f
Jhonny andres ibañez garzon <jhandres187@gmail.com>
2022-10-03 14:31:49 -0500

    ajustes del nav y del top nav

--- resources/views/components/web/navigation-menu.blade.php
+++ resources/views/components/web/navigation-menu.blade.php
@@ -1,64 +1,66 @@
 <nav class="menu-home navbar navbar-expand-lg navbar-dark px-2 px-lg-3 py-0">
-    <a class="navbar-brand" href="#">
-        <figure class="mb-0">
-            <img src="{{ asset('images/Logo-Horizontal-Blanco-y-rojo.png') }}" alt="{{-- $altLogo --}}" class="img-responsive dropFilterLogo">
-        </figure>
-    </a>
-    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#menu-home-collapse" aria-controls="menu-home-collapse"
-        aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
-    <div class="collapse navbar-collapse" id="menu-home-collapse">
-        <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link text-sm" href="{{  route('web.index')  }}">Inicio</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-sm" href="#" id="dropdownWe" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Nosotros
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownWe">
-                    <a class="dropdown-item" href="{{  route('web.ourValues')  }}">Nuestros Valores</a>
-                    <a class="dropdown-item" href="{{  route('web.ourLeaders')  }}">Nuestros Líderes</a>
-                    <a class="dropdown-item" href="{{  route('web.managementReports')  }}">Informes de Gestión</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-sm" href="#" id="dropdownWe" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Ayúdanos a ayudar
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownWe">
-                    <a class="dropdown-item" href="{{  route('web.cashDonations')  }}">Donaciones en efectivo</a>
-                    <a class="dropdown-item" href="{{  route('web.corporateValueProposition')  }}">Propuesta de valor corporativo</a>
-                    <a class="dropdown-item" href="{{  route('web.donationsInKind')  }}">Donaciones en especie</a>
-                    <a class="dropdown-item" href="{{  route('web.volunteering')  }}">Voluntariado</a>
-                    <a class="dropdown-item" href="{{  route('web.academy')  }}">Academia</a>
-                    <a class="dropdown-item" href="{{  route('web.corabastos')  }}">Corabastos</a>
-                    <a class="dropdown-item" href="{{  route('web.prea')  }}">PREA</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-sm" href="#" id="dropdownWe" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Beneficiarios
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownWe">
-                    <a class="dropdown-item" href="{{  route('web.ourWork')  }}">Nuestra Labor</a>
-                    <a class="dropdown-item" href="{{  route('web.linkUp')  }}">Vincúlate</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-sm" href="https://blog.bancodelalimentos.org.co">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-sm" href="#">Regala Generosidad</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-sm" href="{{  route('web.contactUs')  }}">Contáctanos</a>
-            </li>
-            <li class="nav-item">
-                <!-- Button trigger modal -->
-                <a class="nav-link text-sm" type="button" data-bs-toggle="modal" data-bs-target="#modalSearch">
-                    <i class="fas fa-search"></i>
-                </a>
-            </li>
-        </ul>
+    <div class="nav-container flex justify-between">
+        <a class="navbar-brand" href="#">
+            <figure class="mb-0">
+                <img src="{{ asset('images/Logo-Horizontal-Blanco-y-rojo.png') }}" alt="{{-- $altLogo --}}" class="img-responsive dropFilterLogo">
+            </figure>
+        </a>
+        <button class="navbar-toggler d-lg-none border-hidden" type="button" data-bs-toggle="collapse" data-bs-target="#menu-home-collapse" aria-controls="menu-home-collapse"
+            aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
+        <div class="collapse navbar-collapse" id="menu-home-collapse">
+            <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link text-sm" href="{{  route('web.index')  }}">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-sm" href="#" id="dropdownWe" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Nosotros
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownWe">
+                        <a class="dropdown-item" href="{{  route('web.ourValues')  }}">Nuestros Valores</a>
+                        <a class="dropdown-item" href="{{  route('web.ourLeaders')  }}">Nuestros Líderes</a>
+                        <a class="dropdown-item" href="{{  route('web.managementReports')  }}">Informes de Gestión</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-sm" href="#" id="dropdownWe" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Ayúdanos a ayudar
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownWe">
+                        <a class="dropdown-item" href="{{  route('web.cashDonations')  }}">Donaciones en efectivo</a>
+                        <a class="dropdown-item" href="{{  route('web.corporateValueProposition')  }}">Propuesta de valor corporativo</a>
+                        <a class="dropdown-item" href="{{  route('web.donationsInKind')  }}">Donaciones en especie</a>
+                        <a class="dropdown-item" href="{{  route('web.volunteering')  }}">Voluntariado</a>
+                        <a class="dropdown-item" href="{{  route('web.academy')  }}">Academia</a>
+                        <a class="dropdown-item" href="{{  route('web.corabastos')  }}">Corabastos</a>
+                        <a class="dropdown-item" href="{{  route('web.prea')  }}">PREA</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-sm" href="#" id="dropdownWe" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Beneficiarios
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownWe">
+                        <a class="dropdown-item" href="{{  route('web.ourWork')  }}">Nuestra Labor</a>
+                        <a class="dropdown-item" href="{{  route('web.linkUp')  }}">Vincúlate</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-sm" href="https://blog.bancodelalimentos.org.co">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-sm" href="#">Regala Generosidad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-sm" href="{{  route('web.contactUs')  }}">Contáctanos</a>
+                </li>
+                <li class="nav-item">
+                    <!-- Button trigger modal -->
+                    <a class="nav-link text-sm" type="button" data-bs-toggle="modal" data-bs-target="#modalSearch">
+                        <i class="fas fa-search"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
--- resources/views/components/web/top-navigation.blade.php
+++ resources/views/components/web/top-navigation.blade.php
@@ -1,35 +1,37 @@
 <div class="top-nav d-none d-md-flex justify-content-between align-items-center bg-bab px-md-2 px-lg-5">
-    <div class="d-flex justify-content-start py-2">
-        <a href="#">
-            <span class="text-slate-400 hover:text-slate-50 text-sm"><i class="fas fa-shopping-cart"></i> Recompensas</span>
-        </a>
-        <span class="text-gray-300 mx-2"> | </span>
-        <a href="#">
-            <span class="text-slate-400 hover:text-slate-50 text-sm">Bonos</span>
-        </a>
-        <span class="text-gray-300 mx-2"> | </span>
-        <a href="{{  route('login')  }}">
-            <span class="text-slate-400 hover:text-slate-50 text-sm">Inicia Sesion</span>
-        </a>
-        <span class="text-gray-300 mx-2"> | </span>
-    </div>
-    <div class="flex justify-end basis-1/2">
-        <a href="#" class="flex justify-start align-items-center px-5 bg-red-700">
-            <span class="text-white text-lg font-bold">Dona ya!</span>
-        </a>
-        <div class="flex justify-around text-slate-400 hover:text-slate-50 ml-4 mr-8  py-2">
-            <span class="text-slate-50 text-sm">
-                Escribenos
-                <a href="http://" target="_blank" rel="noopener noreferrer" class="text-slate-400 hover:text-slate-50">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a href="http://" target="_blank" rel="noopener noreferrer" class="text-slate-400 hover:text-slate-50">
-                    <i class="fab fa-youtube"></i>
-                </a>
-                <a href="http://" target="_blank" rel="noopener noreferrer" class="text-slate-400 hover:text-slate-50">
-                    <i class="fab fa-instagram"></i>
-                </a>
-            </span>
+    <div class="max-w-top-nav flex justify-between items-center">
+        <div class="d-flex justify-content-start py-2">
+            <a href="#">
+                <span class="text-slate-400 hover:text-slate-50 text-sm"><i class="fas fa-shopping-cart"></i> Recompensas</span>
+            </a>
+            <span class="text-gray-300 mx-2"> | </span>
+            <a href="#">
+                <span class="text-slate-400 hover:text-slate-50 text-sm">Bonos</span>
+            </a>
+            <span class="text-gray-300 mx-2"> | </span>
+            <a href="{{  route('login')  }}">
+                <span class="text-slate-400 hover:text-slate-50 text-sm">Inicia Sesion</span>
+            </a>
+            <span class="text-gray-300 mx-2"> | </span>
+        </div>
+        <div class="flex justify-end basis-1/2">
+            <a href="#" class="flex justify-start align-items-center px-5 bg-red-700">
+                <span class="text-white text-lg font-bold">Dona ya!</span>
+            </a>
+            <div class="flex justify-around text-slate-400 hover:text-slate-50 ml-4 mr-8  py-2">
+                <span class="text-slate-50 text-sm">
+                    Escribenos
+                    <a href="http://" target="_blank" rel="noopener noreferrer" class="text-slate-400 hover:text-slate-50">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer" class="text-slate-400 hover:text-slate-50">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer" class="text-slate-400 hover:text-slate-50">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </span>
+            </div>
         </div>
     </div>
 </div>
