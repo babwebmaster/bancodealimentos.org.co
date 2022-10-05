807a5f6d8d0641f93567369a10bcc6f6dd29ebf8
Jhonny andres ibañez garzon <jhandres187@gmail.com>
2022-10-05 14:30:30 -0500

    refactoring code

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,23 @@
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    var sizeScreen = window.matchMedia("(min-width: 768px)");
-    // sizeScreen.addListener(cambiarMenu)
-    cambiarMenu(sizeScreen) // llama  a la funcion
-    function cambiarMenu(sizeScreen) {
-        if (sizeScreen.matches) { // if media "se cumple"
-            window.onscroll = function (){
-                // Obtenemos la posicion del scroll en pantall
-                var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-                if(scroll > 0){
-                    document.querySelector('.menu-home').classList.add('menu-sticky');
-                }else{
-                    document.querySelector('.menu-home').classList.remove('menu-sticky');
-                }
-            }       
-                
-        }   
-    }
-});
-
-
-
+document.addEventListener("DOMContentLoaded",() => {   
+    const sizeScreen = window.matchMedia("(min-width: 768px)")
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop  ;
+    (scrollTop > 40)?addClassMenu():removeClassMenu();
+    cambiarMenu(sizeScreen) 
+})
+const cambiarMenu = (sizeScreen) => {
+    sizeScreen.matches ? fnScroll :  '';
+}
+const fnScroll = window.onscroll = () => {        
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    (scrollTop > 40)?addClassMenu():removeClassMenu();
+}
+const removeClassMenu = ()=>{
+    const nav = document.querySelector('nav.menu-home')
+    const hasNavClass = nav.classList.contains("menu-sticky")
+    return hasNavClass ? nav.classList.remove("menu-sticky") : '';
+}
+const addClassMenu = ()=>{
+    const nav = document.querySelector('nav.menu-home')
+    const hasNavClass = nav.classList.contains("menu-sticky")
+    return hasNavClass ? '' : nav.classList.add("menu-sticky");
+}
