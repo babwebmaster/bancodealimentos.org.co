3c91e2c97847a14c06c0b361e6808dca7722bc0f
jhonny andres ibañez garzon <jhandres187@gmail.com>
2023-03-06 16:47:11 -0500

    eliminacion total de bootstrap

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -57,8 +57,6 @@
         <livewire:scripts />
         @if (Route::is('web.index'))
             <!-- Bootstrap JavaScript Libraries -->  
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
             <script src="{{ asset('js/dashboard.slideDonors.index.js') }}"></script>
             <script src="{{ asset('js/dashboard.cifras.index.js') }}"></script>
         @endif 
